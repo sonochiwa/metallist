ec667bf9644d0ce6be004c72d7b91ce2729d4a8f
Rashid Mamarimov <sonochiwaxd@gmail.com>
2022-02-24 18:18:50 +0600

    pages upd

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,17 +1,3 @@
-// let newModal = document.getElementById('modal-window');
-
-// if (newModal) {
-//     document.body.style.overflow = "hidden";
-// } else {
-//     document.body.style.overflow = "scroll";
-// }
-
-// document.addEventListener('keydown', function(e) {
-//     if (e.key === 'Escape') {
-//         function hideModal() {
-//             // document.body.style.overflow = "hidden";
-//             newModal.style.display = "none";
-//         }
-//         hideModal()
-//     }
+// document.getElementsByClassName('primary-button').click(function() {
+//     console.log('123');
 // });
